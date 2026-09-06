cca88c0bfb4d89a2afe60bdd6f4c827ee8c85f92
Nanda7923 <nandakoko704@gmail.com>
2026-09-06 17:28:53 +0630

    Add User Schema

---
+++ models/user.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose`;
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 50,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true
+    },
+
+    profile: {
+        type: String,
+        default: ""
+    },
+    dateOfBirth: {
+        type: Date,
+        default: null
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    isBanned: {
+        type: Boolean,
+        default: false
+    },
+    bannedPeriod: {
+        type: Number,
+        default: 1
+    },
+    uploadedCount: {
+        type: Number,
+        default: 0
+    }
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
