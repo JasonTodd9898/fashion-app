3442b264325ffd4b7ac9ed1e33e00ef6b01c88e0
Nanda7923 <nandakoko704@gmail.com>
2026-09-10 08:09:40 +0630

    modification of create_user

--- app/components/CreateUser.jsx
+++ app/components/CreateUser.jsx
@@ -1,32 +1,18 @@
 import "../custom.css";
 
 export default function CreateUser() {
-    const style = {
-        maxWidth: 600,
-        margin: "20px auto",
-    }
-
-    const inputForm = {
-        width: "100%",
-    }
-
-    const signInButton = {
-        width: "80%",
-        backgroundColor: "#6b6dfb",
-        color: "#fff",
-        margin: "0 auto",
-        padding: "8px",
-        border: "none",
-        borderRadius: "8px",
-    }
     return (
-        <form autoComplete="off" className="create-user-form" style={style}>
-            <input type="text" placeholder="Name" className="form-input" autoComplete="off" style={inputForm} />
-            <input type="email" placeholder="Email" className="form-input" autoComplete="new-password" style={inputForm} />
-            <input type="password" placeholder="Password" className="form-input" autoComplete="new-password" style={inputForm} />
+        <form autoComplete="off" className="create-user-form">
+            <input type="text" placeholder="Name" className="form-input" />
+            <input type="email" placeholder="Email" className="form-input" />
+            <input type="password" placeholder="Password" className="form-input" autoComplete="new-password" />
             <label style={{ fontSize: 12, fontWeight: "bold", margin: 0 }}>Date of Birth</label>
-            <input type="date" placeholder="Date of Birth" className="form-input" style={inputForm} />
-            <button type="submit" className="form-button" style={signInButton} >Sign in</button>
+            <input type="date" placeholder="Date of Birth" className="form-input" />
+            <div className="btnStyle">
+                <button type="submit" className="form-button" >Back</button>
+                <button type="submit" className="form-button" >Sign In</button>
+            </div>
+
         </form>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
+import type { CSSProperties } from "react";
 import CreateUser from "./components/CreateUser";
 
 export default function Home() {
-  const logo = {
-    fontSize: 32,
-    fontWeight: "bold",
-    fontStyle: "italic",
-    textAlign: "center",
-    margin: "20px 0"
-  }
   return (
     <div>
-      <h1 style={logo}>Fashion App</h1>
       <CreateUser />
     </div>
   );
